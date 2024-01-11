43081115d3851e4519e0608431c3881e8a7e75c9
jinyr1128 <jinyr1128@gmail.com>
2024-01-11 09:37:22 +0900

    feat : 할일목록만들기기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+export default class App extends Component {
+  btnStyle = {
+    color : "#fff",
+    border:"none",
+    padding : "5px 9px",
+    borderRadius : "50%",
+    cursor:"pointer",
+    float: "right",
+  };
 
-function App() {
-  return (
-    <div>
+  getStyle = () => {
+    return{
+      padding : "10px",
+      borderBottom : "1px #ccc dotted",
+      TextDecoration : "none"
+    }
+  }
 
-    </div>
-  );
+  todoData = [
+    {
+      id: "1",
+      title:"공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title:"청소하기",
+      completed: true
+    }
+  ]
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록 </h1>
+          </div>
+
+          {this.todoData.map((data) =>(
+          <div style={this.listStyle()} key={data.id}>
+            <p>
+            <input type="checkbox" defaultChecked={false}/>
+            {" "}{data.title}
+            <button style={this.btnStyle}>x</button>
+            </p>
+          </div>
+          ))}
+          
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
